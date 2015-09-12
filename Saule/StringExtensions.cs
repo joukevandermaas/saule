1d43f5a89fac77aa058b22aafaeca9fd7585c372
Jouke van der Maas <jouke.vandermaas@gmail.com>
2015-09-12 18:10:55 +0200

    Add string extensions

---
+++ Saule/StringExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Saule
+{
+    internal static class StringExtensions
+    {
+        public static string ToDashed(this string source)
+        {
+            // some-string
+            var parts = SplitAndLower(source);
+
+            return string.Join("-", parts.ToArray());
+        }
+        public static string ToPascalCase(this string source)
+        {
+            // SomeString
+            var parts = SplitAndLower(source);
+
+            return string.Join("", parts.Select(s =>
+                CultureInfo.InvariantCulture.TextInfo.ToTitleCase(s)).ToArray());
+        }
+        public static string ToCamelCase(this string source)
+        {
+            // someString
+            var parts = SplitAndLower(source);
+            var firstWord = parts.FirstOrDefault()?.ToLower();
+
+            var cased = parts.Select((s, i) =>
+            {
+                if (i == 0)
+                    return s.ToLowerInvariant();
+                else
+                    return CultureInfo.InvariantCulture.TextInfo.ToTitleCase(s);
+            });
+
+            return string.Join("", cased.ToArray());
+        }
+
+        private static IEnumerable<string> SplitAndLower(string source)
+        {
+            var strings = new List<string>();
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (IsSeparator(source[i]))
+                {
+                    if (builder.Length > 0)
+                        strings.Add(builder.ToString());
+                    builder.Clear();
+                    continue;
+                }
+                else if (char.IsUpper(source[i]) && builder.Length > 0)
+                {
+                    strings.Add(builder.ToString());
+                    builder.Clear();
+                }
+
+                builder.Append(char.ToLowerInvariant(source[i]));
+            }
+
+            if (builder.Length > 0)
+            {
+                strings.Add(builder.ToString());
+            }
+
+            return strings;
+        }
+
+        private static bool IsSeparator(char value)
+        {
+            return value == '-' || value == '_' || value == ' ';
+        }
+    }
+}
---
+++ Tests/StringExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using Saule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class StringExtensionsTests
+    {
+        private static string[] _camel = { "", "someString", "some", "someVeryLongString" };
+        private static string[] _pascal = { "", "SomeString", "Some", "SomeVeryLongString" };
+        private static string[] _dashed = { "", "some-string", "some", "some-very-long-string" };
+        private static string[] _spaced = { "", "some string", "some", "Some very Long string" };
+        private static string[] _traps = { "----", "   some---String    ", "_- some ", "_some_very-longString" };
+
+        [Fact(DisplayName ="Camel case works")]
+        public void TestCamel()
+        {
+            TestCasing(_camel, StringExtensions.ToCamelCase);
+        }
+        [Fact(DisplayName ="Pascal case works")]
+        public void TestPascal()
+        {
+            TestCasing(_pascal, StringExtensions.ToPascalCase);
+        }
+        [Fact(DisplayName ="Dashed works")]
+        public void TestDashed()
+        {
+            TestCasing(_dashed, StringExtensions.ToDashed);
+        }
+
+        private void TestCasing(string[] array, Func<string, string> function)
+        {
+            for(int i = 0;i < array.Length; i++)
+            {
+                Assert.Equal(array[i], function(_camel[i]));
+                Assert.Equal(array[i], function(_pascal[i]));
+                Assert.Equal(array[i], function(_dashed[i]));
+                Assert.Equal(array[i], function(_spaced[i]));
+                Assert.Equal(array[i], function(_traps[i]));
+            }
+        }
+    }
+}
