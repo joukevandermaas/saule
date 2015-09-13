57194d246ad3c350ff8a45358c3c0918d6b5862b
Jouke van der Maas <jouke.vandermaas@gmail.com>
2015-09-13 16:32:18 +0200

    wip deserialization

---
+++ Saule/Serialization/JsonApiDeserializer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace Saule.Serialization
+{
+    class JsonApiDeserializer
+    {
+        public T Deserialize<T>(JToken json)
+        {
+            return ToFlatStructure(json).ToObject<T>();
+        }
+
+        private JToken ToFlatStructure(JToken json)
+        {
+            var array = json["data"] as JArray;
+            if(array != null)
+            {
+                var result = new JArray();
+
+                foreach (var child in array)
+                {
+                    result.Add(SingleToFlatStructure(child as JObject));
+                }
+                return result;
+            }
+            else
+            {
+                return SingleToFlatStructure(json["data"] as JObject);
+            }
+        }
+
+        private JToken SingleToFlatStructure(JObject child)
+        {
+            var result = new JObject();
+            result.Add(child.Property("id"));
+
+            foreach (var attr in child["attributes"] ?? new JArray())
+                result.Add(attr);
+
+            foreach (var rel in child["relationships"] ?? new JArray())
+                result.Add((rel as JProperty).Name, ToFlatStructure(rel.First));
+
+            return result;
+        }
+    }
+}
--- Tests/Experiments.cs
+++ Tests/Experiments.cs
@@ -1,6 +1,7 @@
-﻿using Saule;
-using System;
+﻿using Newtonsoft.Json.Linq;
+using Saule.Serialization;
 using System.Collections.Generic;
+using Tests.SampleModels;
 using Xunit;
 
 namespace Tests
@@ -10,7 +11,20 @@ namespace Tests
         [Fact]
         public void Run()
         {
-            //var obj = Activator.CreateInstance(typeof(PersonModel));
+            var person = new Person(false)
+            {
+                Id = "aaa",
+                FirstName = "Adam",
+                LastName = "Johnson",
+                Age = 54,
+
+                Friends = new[] { new Person(prefill: true) },
+                Job = new Company(prefill: true)
+            };
+            var json = new JsonApiSerializer().Serialize(new ApiResponse(person, new PersonResource()), "/");
+            var target = new JsonApiDeserializer();
+
+            var result = target.Deserialize<Person>(json);
         }
     }
 }
