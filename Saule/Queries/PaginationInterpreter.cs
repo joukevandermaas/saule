f81fe3709b146f9366cb33f31e02bcff57a9d2e3
Jouke van der Maas <jouke.vandermaas@gmail.com>
2015-10-25 22:15:20 +0100

    Support real queryables
    Real queryables apparently do not support `Skip` unless you call
    `OrderBy` first, so now we order before we call `Skip` if we have a
    queryable.

--- Saule/Queries/PaginationInterpreter.cs
+++ Saule/Queries/PaginationInterpreter.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.CodeDom;
+using System.Collections;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Saule.Queries
 {
@@ -14,11 +18,44 @@ namespace Saule.Queries
 
         public IQueryable Apply(IQueryable queryable)
         {
-            var filtered = queryable.ApplyQuery(QueryMethod.Skip, Context.Page * Context.PerPage) as IQueryable;
+            // Skip does not work on queryables by default, because it makes
+            // no sense if the order is not determined. This means we have to
+            // order the queryable first, before we can apply pagination.
+            var ordered = typeof(IOrderedQueryable<>).IsAssignableFrom(queryable.Expression.Type)
+                ? OrderById(queryable)
+                : queryable;
+
+            var filtered = ordered.ApplyQuery(QueryMethod.Skip, Context.Page * Context.PerPage) as IQueryable;
             filtered = filtered.ApplyQuery(QueryMethod.Take, Context.PerPage) as IQueryable;
 
             return filtered;
         }
+
+        private static IQueryable OrderById(IQueryable queryable)
+        {
+            var funcType = typeof(Func<,>).MakeGenericType(queryable.ElementType, typeof(object));
+            var param = Expression.Parameter(queryable.ElementType, "i");
+            var property = Expression.Property(param, "Id");
+
+            var expressionFactory = typeof(Expression).GetMethods()
+                .Where(m => m.Name == "Lambda")
+                .Select(m => new
+                {
+                    Method = m,
+                    Params = m.GetParameters(),
+                    Args = m.GetGenericArguments()
+                })
+                .Where(x => x.Params.Length == 2 && x.Args.Length == 1)
+                .Select(x => x.Method)
+                .First()
+                .MakeGenericMethod(funcType);
+
+            var expression = expressionFactory.Invoke(null, new object[] { property, new[] { param } });
+
+            var ordered = queryable.ApplyQuery(QueryMethod.OrderBy, expression) as IQueryable;
+            return ordered;
+        }
+
         public IEnumerable Apply(IEnumerable queryable)
         {
             var filtered = queryable.ApplyQuery(QueryMethod.Skip, Context.Page * Context.PerPage) as IEnumerable;
--- Saule/Queries/QueryMethod.cs
+++ Saule/Queries/QueryMethod.cs
@@ -7,16 +7,19 @@ using System.Reflection;
 
 namespace Saule.Queries
 {
-    internal sealed class QueryMethod
+    internal class QueryMethod
     {
         // This idea was borrowed from the OData repo. See the following url:
         // https://github.com/OData/WebApi/blob/master/OData/src/System.Web.Http.OData/OData/ExpressionHelperMethods.cs 
         public static QueryMethod Skip => new QueryMethod(
             GetGenericMethodInfo(_ => default(IQueryable<int>).Skip(default(int))),
             GetGenericMethodInfo(_ => default(IEnumerable<int>).Skip(default(int))));
+
         public static QueryMethod Take => new QueryMethod(
             GetGenericMethodInfo(_ => default(IQueryable<int>).Take(default(int))),
             GetGenericMethodInfo(_ => default(IEnumerable<int>).Take(default(int))));
+
+        public static QueryMethod OrderBy => new OrderByQueryMethod();
 
         private readonly MethodInfo _enumerable;
         private readonly MethodInfo _queryable;
@@ -37,7 +40,7 @@ namespace Saule.Queries
             return ApplyToInternal(_enumerable, new[] { enumerable }.Concat(arguments).ToArray());
         }
 
-        private static object ApplyToInternal(MethodInfo method, object[] arguments)
+        protected virtual object ApplyToInternal(MethodInfo method, object[] arguments)
         {
             var typed = method.MakeGenericMethod(arguments[0].GetType().GenericTypeArguments);
             return typed.Invoke(null, arguments);
@@ -52,6 +55,24 @@ namespace Saule.Queries
             var lambdaExpression = expression as LambdaExpression;
 
             return (lambdaExpression?.Body as MethodCallExpression)?.Method.GetGenericMethodDefinition();
+        }
+
+        private class OrderByQueryMethod : QueryMethod
+        {
+            public OrderByQueryMethod() : base(
+                GetGenericMethodInfo(_ => default(IQueryable<int>).OrderBy(default(Expression<Func<int, int>>))),
+                GetGenericMethodInfo(_ => default(IQueryable<int>).OrderBy(default(Expression<Func<int, int>>))))
+            { }
+
+            protected override object ApplyToInternal(MethodInfo method, object[] arguments)
+            {
+                // Type params are the same as the func in the expression.
+                // (arguments[0] is Expression<Func<SomeType, object>>)
+                var typeParams = arguments[1].GetType().GenericTypeArguments[0].GenericTypeArguments;
+                var typed = method.MakeGenericMethod(typeParams);
+
+                return typed.Invoke(null, arguments);
+            }
         }
     }
 }
