364e320c1a413c716c47bcb450e83de8770c9e47
Jouke van der Maas <jouke.vandermaas@gmail.com>
2015-12-07 21:01:29 +0100

    Introduce random names to tests
    Just to make sure we don't implicitly rely on fixed values anywhere.

--- Tests/Helpers/Get.cs
+++ Tests/Helpers/Get.cs
@@ -7,26 +7,37 @@ namespace Tests.Helpers
 {
     internal static class Get
     {
+        private static readonly Random random = new Random();
+        private static readonly string[] FirstNames =
+        {
+            "Ola", "Lisabeth", "Latoya", "Sabina", "Eugenie", "Francisco",
+            "Kenneth", "Teofila", "Honey", "Larissa", "Dannie", "Nery", "Oswaldo",
+            "Romona", "Ollie", "Vergie", "Lolita", "Jung", "Sheba", "Fonda",
+        };
+
+        private static readonly string[] LastNames =
+        {
+            "Summers", "Bockman", "Duque", "Cline", "Neufeld", "Mcray", "Hix",
+            "Daniel", "Baumbach", "Forry", "Bozek", "Chichester", "Petri", "Folk",
+            "Yadon", "Holliday", "Paniagua", "Hofstetter", "Vasques", "Russel"
+        };
+
         public static IEnumerable<Person> People()
         {
             var i = 0;
-            var random = new Random();
-
             while (true)
             {
-                yield return new Person(prefill: true, id: i++.ToString())
-                {
-                    Age = random.Next(80)
-                };
+                yield return Person(i++.ToString());
             }
         }
+
         public static IEnumerable<Company> Companies()
         {
             var i = 0;
 
             while (true)
             {
-                yield return new Company(prefill: true, id: i++.ToString());
+                yield return Company(i++.ToString());
             }
         }
 
@@ -41,7 +52,12 @@ namespace Tests.Helpers
 
         public static Person Person(string id = "123")
         {
-            return new Person(prefill: true, id: id);
+            return new Person(prefill: true, id: id)
+            {
+                Age = random.Next(80),
+                FirstName = FirstNames[random.Next(FirstNames.Length)],
+                LastName = LastNames[random.Next(LastNames.Length)]
+            };
         }
         public static Company Company(string id = "456")
         {
