86d7c62de496def9a6722238ad9e94e8f01f5e0c
Jouke van der Maas <jouke.vandermaas@gmail.com>
2016-01-10 23:21:47 +0100

    Find url namespace by default
    This makes the 'prefix' parameter unnecessary for
    `DefaultUrlPathBuilder` in 90% of cases. Fixes #71.

--- Tests/Controllers/BrokenController.cs
+++ Tests/Controllers/BrokenController.cs
@@ -7,7 +7,7 @@ namespace Tests.Controllers
     public class BrokenController : ApiController
     {
         [HttpGet]
-        [Route("broken/{id}")]
+        [Route("api/broken/{id}")]
         public Person GetPerson(string id)
         {
             return Get.Person(id);
