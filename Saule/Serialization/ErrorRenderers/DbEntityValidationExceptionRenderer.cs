cf1250066c239db733fc70b84705a2818cef11b3
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-04-14 12:02:36 -0400

    failing code for review / help - error renderers for different exception types

---
+++ Saule/Serialization/ErrorRenderers/DbEntityValidationExceptionRenderer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Saule.Serialization.ErrorRenderers
+{
+    internal class DbEntityValidationExceptionRenderer : ExceptionRenderer
+    {
+        private DbEntityValidationException _exception;
+
+
+        public DbEntityValidationExceptionRenderer(DbEntityValidationException validationException)
+        {
+            _exception = validationException;
+        }
+
+        public override JArray ToJObject()
+        {
+            var result = new JArray();
+
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            foreach (var dbError in _exception.EntityValidationErrors)
+            {
+                foreach (var dbErrorEntry in dbError.ValidationErrors)
+                {
+                    var error = AsError(dbError, dbErrorEntry);
+                    result.Add(error);
+                }
+
+            }
+
+
+            return result;
+        }
+
+
+        private object AsError(DbEntityValidationResult dbError, DbValidationError dbErrorEntry)
+        {
+            var status = "422";
+            var detail = dbErrorEntry.ErrorMessage;
+            var source = new { pointer = "/data/attributes/" + dbErrorEntry.PropertyName };
+
+            return new { status = status, detail = detail, source = source };
+        }
+    }
+}
--- Saule/Serialization/ErrorSerializer.cs
+++ Saule/Serialization/ErrorSerializer.cs
@@ -7,16 +7,8 @@ namespace Saule.Serialization
     {
         public JObject Serialize(ApiError error)
         {
-            var result = new JObject();
-
-            var json = JObject.FromObject(error, new JsonSerializer { NullValueHandling = NullValueHandling.Ignore });
-
-            foreach (var token in json)
-            {
-                result.Add(token.Key.ToCamelCase(), token.Value);
-            }
-
-            return new JObject { ["errors"] = new JArray { result } };
+            var result = error.ToJObject();
+            return new JObject { ["errors"] = result };
         }
     }
 }
