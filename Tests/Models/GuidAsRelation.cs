a955204fd3fa5f6f51f02d58c9bfb681b376db53
Eltjo <eltjo@curit.com>
2016-04-23 17:25:45 +0200

    add failing tests for Guid relations

---
+++ Tests/Models/GuidAsRelation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Tests.Models
+{
+    internal class GuidAsRelation
+    {
+        public GuidAsRelation(string id = "938")
+        {
+            Id = id;
+            Relation = new GuidAsId();
+            Relations = new [] { new GuidAsId(), new GuidAsId() };
+        }
+
+        public string Id { get; set; }
+
+        public GuidAsId Relation { get; set; }
+
+        public IEnumerable<GuidAsId> Relations {get; set;}
+    }
+}
