402302b68acc45b88661d3cfcda3a825aa38d22b
Matt Bird <matthew.bird@icarehealth.co.uk>
2016-06-15 11:04:57 +0100

    Fix for #111 (500 error for non-API endpoints) and #116 (PreprocessingDelegatingHandler breaks Swagger UI).

--- Tests/Helpers/Paths.cs
+++ Tests/Helpers/Paths.cs
@@ -2,7 +2,7 @@
 {
     public static class Paths
     {
-        public const string StaticContent = "static/text";
+        public const string StaticText = "static/text";
         public const string SingleResource = "api/people/123/";
         public const string ResourceCollection = "api/people/";
         public const string NonExistingPath = "api/wrong/";
