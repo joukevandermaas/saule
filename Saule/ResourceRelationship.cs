bba318ee7c2dffd64aa716818d87d1df95129f56
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2017-05-30 10:08:42 +0200

    Optional links support (#153)
    * Work in progress optional links support
    
    * Additional tests
    
    * Add documentation
    
    * Remove RelatedCanonical
    
    * Added newlines

--- Saule/ResourceRelationship.cs
+++ Saule/ResourceRelationship.cs
@@ -15,17 +15,20 @@
         /// </param>
         /// <param name="kind">The kind of relationship.</param>
         /// <param name="relationshipResource">The specification of the related resource.</param>
+        /// <param name="withLinks">The defined <see cref="LinkType"/> to be generated for this relationship.</param>
         protected ResourceRelationship(
             string name,
             string urlPath,
             RelationshipKind kind,
-            ApiResource relationshipResource)
+            ApiResource relationshipResource,
+            LinkType withLinks)
         {
             Name = name.ToDashed();
             PropertyName = name.ToPascalCase();
             UrlPath = urlPath.ToDashed();
             RelatedResource = relationshipResource;
             Kind = kind;
+            LinkType = withLinks;
         }
 
         /// <summary>
@@ -52,5 +55,10 @@
         /// Gets the kind of relationship.
         /// </summary>
         public RelationshipKind Kind { get; }
+
+        /// <summary>
+        /// Gets the defined <see cref="LinkType"/> to be generated for this relationship.
+        /// </summary>
+        public LinkType LinkType { get; }
     }
 }
---
+++ Tests/Models/PersonNoJobLinksResource.cs
@@ -0,0 +1,22 @@
+﻿using Saule;
+
+namespace Tests.Models
+{
+    public class PersonNoJobLinksResource : ApiResource
+    {
+        public PersonNoJobLinksResource()
+        {
+            WithId(nameof(Person.Identifier));
+
+            Attribute(nameof(Person.FirstName));
+            Attribute(nameof(Person.LastName));
+            Attribute(nameof(Person.Age));
+            Attribute(nameof(Person.Address));
+
+            BelongsTo<CompanyResource>(nameof(Person.Job), "/employer", LinkType.None);
+            BelongsTo<CarResource>(nameof(Person.Car));
+            HasMany<PersonResource>(nameof(Person.Friends));
+            HasMany<PersonResource>( nameof( Person.FamilyMembers));
+        }
+    }
+}
