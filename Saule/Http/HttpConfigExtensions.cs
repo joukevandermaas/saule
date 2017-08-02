acecd0d2c6eb5d4bf2745ba6a45b4c67ade88ee0
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2017-08-02 08:36:06 +0200

    configureJsonApi overload (formatter can be only add at end of the collection, without delete other formatters)

--- Saule/Http/HttpConfigExtensions.cs
+++ Saule/Http/HttpConfigExtensions.cs
@@ -48,18 +48,49 @@ namespace Saule.Http
             JsonApiConfiguration jsonApiConfiguration,
             bool overwriteOtherFormatters)
         {
+            if (overwriteOtherFormatters)
+            {
+                ConfigureJsonApi(config, jsonApiConfiguration, ConfigureFormattersEnum.OverwriteOtherFormatters);
+            }
+            else
+            {
+                ConfigureJsonApi(config, jsonApiConfiguration, ConfigureFormattersEnum.AddFormatterToStart);
+            }
+
+        }
+
+        /// <summary>
+        ///  Sets up serialization and deserialization of Json Api resources.
+        /// </summary>
+        /// <param name="config">The <see cref="HttpConfiguration"/> that is used in the setup of the application.</param>
+        /// <param name="jsonApiConfiguration">Configuration parameters for Json Api serialization.</param>
+        /// <param name="configureFormatters">
+        /// AddFormatterToStart - Formatter will be inserted at the start of the collection.
+        /// AddFormatterToEnd - Formatter will be inserted at the end of the collection.
+        /// OverwriteOtherFormatters -  Other formatters will be cleared.
+        /// </param>
+        public static void ConfigureJsonApi(
+          this HttpConfiguration config,
+          JsonApiConfiguration jsonApiConfiguration,
+          ConfigureFormattersEnum configureFormatters)
+        {
             config.MessageHandlers.Add(new PreprocessingDelegatingHandler(jsonApiConfiguration));
             var formatter = new JsonApiMediaTypeFormatter(jsonApiConfiguration);
 
-            if (overwriteOtherFormatters)
+            if (configureFormatters == ConfigureFormattersEnum.OverwriteOtherFormatters)
             {
                 config.Formatters.Clear();
                 config.Formatters.Add(formatter);
             }
-            else
+            else if (configureFormatters == ConfigureFormattersEnum.AddFormatterToEnd)
+            {
+                config.Formatters.Add(formatter);
+            }
+            else if (configureFormatters == ConfigureFormattersEnum.AddFormatterToStart)
             {
                 config.Formatters.Insert(0, formatter);
             }
         }
+
     }
 }
