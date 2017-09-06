77157412283bb996b9e8b559ba99d2920cae4d1a
Laurence Hartgill <laurence@hartgill.co.uk>
2017-09-06 18:02:21 +0100

    Convert Formatter to Input and Output Formatters

---
+++ Saule.NetCore/Http/Formatters/JsonApiInputFormatter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Saule.Serialization;
+
+namespace Saule.Http.Formatters
+{
+    /// <summary>
+    /// Custom media type formatter for Json Api (1.0) responses and requests.
+    /// </summary>
+    public class JsonApiInputFormatter : TextInputFormatter
+    {
+        private readonly JsonApiConfiguration _config = new JsonApiConfiguration();
+
+        internal JsonApiInputFormatter(JsonApiConfiguration config)
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue(Constants.MediaType));
+            _config = config;
+
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+        }
+
+        /// <inheritdoc/>
+        public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        => ReadRequestBodyAsync(context, Encoding.UTF8);
+
+        /// <inheritdoc/>
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+        {
+            var readStream = context.HttpContext.Request.Body;
+
+            using (var reader = new StreamReader(readStream, encoding))
+            {
+                try
+                {
+                    var json = JToken.Parse(await reader.ReadToEndAsync());
+                    var obj = new ResourceDeserializer(json, context.ModelType).Deserialize();
+                    return InputFormatterResult.Success(obj);
+                }
+                catch (JsonApiException ex)
+                {
+                    context.ModelState.AddModelError("Request content", ex.Message);
+                    return InputFormatterResult.Failure();
+                }
+                catch (JsonReaderException ex)
+                {
+                    context.ModelState.AddModelError("Request content", ex.Message);
+                    return InputFormatterResult.Failure();
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override bool CanReadType(Type type)
+        {
+            return true;
+        }
+    }
+}
---
+++ Saule.NetCore/Http/Formatters/JsonApiOutputFormatter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Saule.Http.Formatters
+{
+    /// <summary>
+    /// Custom media type formatter for Json Api (1.0) responses and requests.
+    /// </summary>
+    public class JsonApiOutputFormatter : TextOutputFormatter
+    {
+        private readonly JsonApiConfiguration _config = new JsonApiConfiguration();
+
+        internal JsonApiOutputFormatter(JsonApiConfiguration config)
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue(Constants.MediaType));
+            _config = config;
+
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+        }
+
+        /// <inheritdoc/>
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var preprocessed = context.HttpContext.Items[Constants.PropertyNames.PreprocessResult]
+                as PreprocessResult;
+
+            var json = JsonApiSerializer.Serialize(preprocessed);
+
+            var response = context.HttpContext.Response;
+            return WriteJsonToStream(json, response.Body);
+        }
+
+        /// <inheritdoc/>
+        protected override bool CanWriteType(Type type)
+        {
+            return true;
+        }
+
+        private async Task WriteJsonToStream(JToken json, Stream stream)
+        {
+            using (var writer = new StreamWriter(stream, Encoding.UTF8, 2048, true))
+            {
+                await writer.WriteAsync(json.ToString(Formatting.None, _config.JsonConverters.ToArray()));
+            }
+        }
+    }
+}
