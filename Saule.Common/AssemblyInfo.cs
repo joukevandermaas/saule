5036022c09938aae26cca6b6d62ea9e483cd28e4
Laurence Hartgill <laurence@hartgill.co.uk>
2017-09-07 09:39:52 +0100

    Extract common code to common library

---
+++ Saule.Common/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+
+[assembly: InternalsVisibleTo("Saule")]
+[assembly: InternalsVisibleTo("Tests")]
+[assembly: InternalsVisibleTo("Saule.NetCore")]
+[assembly: InternalsVisibleTo("Saule.NetCore.Tests")]
+[assembly: InternalsVisibleTo("Saule.Common.Tests")]
