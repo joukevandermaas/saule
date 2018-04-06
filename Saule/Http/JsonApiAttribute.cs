cb5545fb81cb6357948eca95cc1f8de89fe00938
barsh <michael.barshinger@profiscience.com>
2018-04-06 17:00:58 -0500

    wip

--- Saule/Http/JsonApiAttribute.cs
+++ Saule/Http/JsonApiAttribute.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Web.Http.Filters;
 
 namespace Saule.Http
@@ -9,31 +7,15 @@ namespace Saule.Http
     /// An optional attribute that can be used to opt an api into returning a JsonApi response.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
-    public sealed class JsonApiAttribute : ActionFilterAttribute
+    public class JsonApiAttribute : ActionFilterAttribute
     {
         /// <summary>
         /// See base class documentation.
         /// </summary>
-        /// <param name="actionExecutedContext">The action context.</param>
-        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        /// <param name="context">The action context.</param>
+        public override void OnActionExecuted(HttpActionExecutedContext context)
         {
-            if (actionExecutedContext.Exception != null)
-            {
-                return;
-            }
-
-            var content = actionExecutedContext.Response.Content as ObjectContent;
-            if (content == null)
-            {
-                return;
-            }
-
-            var formatter = new JsonApiMediaTypeFormatter(new JsonApiConfiguration()).GetPerRequestFormatterInstance(
-                typeof(string),
-                actionExecutedContext.Request,
-                new MediaTypeHeaderValue(Constants.MediaType));
-
-            actionExecutedContext.Response.Content = new ObjectContent(content.ObjectType, content.Value, formatter);
+            JsonApiProcessor.ProcessRequest(context);
         }
     }
 }
---
+++ Saule/Http/JsonApiProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http.Filters;
+using Saule.Serialization;
+
+namespace Saule.Http
+{
+    /// <summary>
+    /// Processes JSON API responses
+    /// </summary>
+    public class JsonApiProcessor
+    {
+        /// <summary>
+        /// process request as JSON API response
+        /// </summary>
+        /// <param name="context">context</param>
+        public static void ProcessRequest(HttpActionExecutedContext context)
+        {
+            var statusCode = (int)context.Response.StatusCode;
+            if (statusCode >= 400 && statusCode < 500)
+            {
+                // probably malformed request or not found
+                return;
+            }
+
+            var value = context.Response.Content as ObjectContent;
+
+            var config = new JsonApiConfiguration();
+
+            var content = PreprocessingDelegatingHandler.PreprocessRequest(value?.Value, context.Request, config);
+
+            if (content.ErrorContent != null)
+            {
+                context.Response.StatusCode = ApiError.IsClientError(content.ErrorContent)
+                    ? HttpStatusCode.BadRequest
+                    : HttpStatusCode.InternalServerError;
+            }
+
+            context.Request.Properties.Add(Constants.PropertyNames.PreprocessResult, content);
+
+            if (context.Exception != null)
+            {
+                return;
+            }
+
+            var responseContent = context.Response.Content as ObjectContent;
+            if (responseContent == null)
+            {
+                return;
+            }
+
+            var formatter = new JsonApiMediaTypeFormatter(new JsonApiConfiguration()).GetPerRequestFormatterInstance(
+                typeof(string),
+                context.Request,
+                new MediaTypeHeaderValue(Constants.MediaType));
+
+            context.Response.Content = new ObjectContent(responseContent.ObjectType, responseContent.Value, formatter);
+        }
+    }
+}
