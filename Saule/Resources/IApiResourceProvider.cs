78c34013680a7a768363604ce5601db89c94b995
Sergey Litvinov <sergey.litvinov@booker.com>
2020-08-20 15:27:15 +0300

    Updated IApiResourceProvider to care about relationship\include cases

--- Saule/Resources/IApiResourceProvider.cs
+++ Saule/Resources/IApiResourceProvider.cs
@@ -11,5 +11,13 @@
         /// <param name="dataObject">Data object that is serialized. If it's an array, then it will be called for each item in the array</param>
         /// <returns>ApiResource that should be used for specific data object</returns>
         ApiResource Resolve(object dataObject);
+
+        /// <summary>
+        /// Returns ApiResource based on the data object and specific relationship
+        /// </summary>
+        /// <param name="dataObject">Data object that is serialized. If it's an array, then it will be called for each item in the array</param>
+        /// <param name="relationship">Relationship resource for which we are resolving api resource</param>
+        /// <returns>ApiResource that should be used for specific data object and relationship</returns>
+        ApiResource ResolveRelationship(object dataObject, ApiResource relationship);
     }
 }
